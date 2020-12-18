047f14fd019dbdfd9302558ff1659fda31c7b3ba
王波 <1294945369@qq.com>
2020-12-18 11:06:40 +0800

    存档

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request (config) {
   // 1. 创建axios的实例
   const instance = axios.create({
-    baseURL: 'mtop.taobao.baichuan.smb.get',
+    baseURL: '',
     timeout: 5000
   })
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,7 +6,12 @@
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
     <feature-view />
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+      @tabClick="tabClick"
+    />
+    <good-list :goods="showGoods" />
     <ul>
       <li>1</li>
       <li>1</li>
@@ -119,34 +124,82 @@ import FeatureView from './childComps/FeatureView'
 
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
+import GoodList from 'components/content/goods/GoodsList'
 
 import { getHomeMultidata, getHomeGoods } from 'network/home'
 export default {
   name: 'Home',
-  components: { HomeSwiper, RecommendView, FeatureView, NavBar, TabControl },
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodList
+  },
   data () {
     return {
       banners: [],
       recommends: [],
       goods: {
         pop: { page: 0, list: [] },
-        news: { page: 0, list: [] },
+        new: { page: 0, list: [] },
         sell: { page: 0, list: [] }
-      }
+      },
+      currentType: 'pop'
+    }
+  },
+  computed: {
+    showGoods () {
+      return this.goods[this.currentType].list
     }
   },
   created () {
     // 1. 请求多个数据
-    getHomeMultidata().then(res => {
-      // this.result = res
-      this.banners = res.data.banner.list
-      this.recommends = res.data.recommend.list
-    })
+    this.getHomeMultidata()
 
     // 2. 请求商品数据
-    getHomeGoods('pop', 1).then(res => {
-      console.log(res)
-    })
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    /**
+     * 事件监听相关的方法
+     */
+    tabClick (index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+    },
+
+    /**
+     * 网络请求相关的方法
+     */
+
+    getHomeMultidata () {
+      getHomeMultidata().then(res => {
+        // this.result = res
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    },
+    getHomeGoods (type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        // console.log(res.data)
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    }
   }
 }
 </script>
@@ -168,5 +221,6 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+  z-index: 9;
 }
 </style>
