5e6e306fba08b3f7b80adeaa92219f9d6b2fc24e
王波 <1294945369@qq.com>
2020-12-18 08:08:21 +0800

    存档

---
+++ src/views/home/Home.vue
@@ -0,0 +1,172 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
+import { getHomeMultidata, getHomeGoods } from 'network/home'
+export default {
+  name: 'Home',
+  components: { HomeSwiper, RecommendView, FeatureView, NavBar, TabControl },
+  data () {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        news: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      }
+    }
+  },
+  created () {
+    // 1. 请求多个数据
+    getHomeMultidata().then(res => {
+      // this.result = res
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+
+    // 2. 请求商品数据
+    getHomeGoods('pop', 1).then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style scoped>
+#home {
+  padding-top: 44px;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+}
+</style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="feature">
+    <a href="https://www.bilibili.com/video/BV17j411f74d?p=158">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureView'
+}
+</script>
+
+<style scoped>
+.feature img {
+  width: 100%;
+}
+</style>
